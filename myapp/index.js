d69e4211b4e2dabaa0ec642a747604e57b96de6c
Javeria-Zaheer <41473@students.riphah.edu.pk>
2025-05-08 14:16:02 +0500

    Merge remote and local changes

--- myapp/index.js
+++ myapp/index.js
@@ -1,12 +1,6 @@
-const express= require('express')
-const app = express()
-app.get('/', (req, res) => {
-    res.send('Dockerise image deployed on heroku');
-});
-app.get('/hello', (req, res) => {
-    res.send('Hello fromDockerise image deployed on heroku');
-});
-app.listen(3001, ()=>{
-    console.log("Backend server running on port 3001")
-    
-})
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('Hello from Railway!'));
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
