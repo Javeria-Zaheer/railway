3a04f075d8e9bca85364da5f4f01b843fe901045
Javeria-Zaheer <41473@students.riphah.edu.pk>
2025-05-08 14:29:51 +0500

    Merge remote and local changes

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello from Railway!'));
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
